7b1f7b2d0b85e3120c9ef2012b0d4bd18bde2a26
JulioM <jucemalco@gmail.com>
2022-03-16 16:03:18 -0300

    update about decorations and path created

--- app.py
+++ app.py
@@ -2,16 +2,66 @@ import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import db, User, Categories, Books, Favorite, Match 
+from models import db, User, Categories, Favorite, Match, Product
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_bcrypt import Bcrypt
+
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASEDIR, "app.db")
+app.config["SECRET_KEY"] = "secret-key"
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
+#PARA VER SI LA APP ESTA FUNCIONANDO
+@app.route("/")
+def home():
+    return "<h1>Hello Api </h1>"
+
+
+#Iniciar Sesión
+@app.route("/login", methods=["POST"])
+def login():
+    password = request.json.get("password")
+    email = request.json.get("email")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is not None:
+        if bcrypt.check_password_hash(user.password, password):
+            access_token = create_access_token(identity=email)
+            return jsonify({
+                "access_token": access_token,
+                "user": user.serialize(),
+                "success":True
+            }), 200
+        else:
+            return jsonify({
+                "msg": "Correo o email inválido"
+            })
+    else:
+          return jsonify({
+                "msg": "Registrate"
+            })    
+    
+
+
+# OBTENER PERFIL
+@app.route("/get_profile")
+@jwt_required()
+def get_profile():
+    user = User.query.get()
+    return jsonify({
+        "user":user.serialize()
+    })
+
+# PARA CREAR USUARIO
 @app.route("/user", methods = ["POST"])
 def create_user():
     user = User()
@@ -22,13 +72,14 @@ def create_user():
 
     if user.name == "":
         return jsonify({
-            "msg": "Name can not be empty"
+            "msg": "El campo nombre no puede estar vacío"
         }), 400 
 
     if user.email == "":
          return jsonify({
-            "msg": "Name can not be empty"
+            "msg": "Este campo no puede estar vacío"
         }), 400 
+    
 
     db.session.add(user)
     db.session.commit()
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250),nullable=False)
-    nickname = db.Column(db.String(250),nullable=False)
+    surname = db.Column(db.String(250),nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     
@@ -16,7 +16,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "nickname": self.nickname,
+            "surname": self.surname,
             "email": self.email,
         }
 
@@ -36,26 +36,26 @@ class Categories(db.Model):
             "description": self.description,
         }
 
-class Books(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250),nullable=False)
-    size = db.Column(db.String(250),nullable=False)
-    description = db.Column(db.String(250),nullable=False)
-    categories = db.Column(db.String(250),nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+#class Books(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250),nullable=False)
+#     size = db.Column(db.String(250),nullable=False)
+#     description = db.Column(db.String(250),nullable=False)
+#     categories = db.Column(db.String(250),nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
 
-    def _repr_ (self):
-        return '<User %r>' % self.name
+#     def _repr_ (self):
+#         return '<User %r>' % self.name
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "size": self.size,
-            "description": self.description,
-            "categories": self.categories,
-            "user.id": self.user_id,
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "size": self.size,
+#             "description": self.description,
+#             "categories": self.categories,
+#             "user.id": self.user_id,
+#        }
     
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -84,4 +84,21 @@ class Match(db.Model):
     "id" : self.id,
     "user_id": self.user_id,
     "book:id": self.book_id
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    autor = db.Column(db.String(10), nullable=False)
+    editorial = db.Column(db.String(10), nullable=False)
+    
+    def _repr_(self):
+        return "<Product %r>" % self.title
+
+    def serialize(self):
+        return {
+            "id":self.id,
+            "title": self.title,
+            "autor":self.autor,
+            "editorial": self.editorial
         }
